97c49cdece4025fb564ad068fe6425a6246a9967
skinnerthe3rd <99153331+skinnerthe3rd@users.noreply.github.com>
2022-02-12 22:03:27 +0000

    Add files via upload

---
+++ skinner2/keymap.c
@@ -0,0 +1,85 @@
+#include QMK_KEYBOARD_H
+
+
+#define FN1_SPC     LT(1, KC_SPC)
+#define FN2_BSPC    LT(2, KC_BSPC)
+#define FN3_C       LT(3, KC_C)
+#define FN4_V       LT(4, KC_V)
+#define FN5_B       LT(5, KC_B)
+#define CTL_Z       CTL_T(KC_Z)
+#define ALT_X       ALT_T(KC_X)
+#define ALT_N       ALGR_T(KC_N)
+#define CTL_M       RCTL_T(KC_M)
+#define SFT_ENT     RSFT_T(KC_ENT)
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+  [0] = LAYOUT_ortho_3x10(
+    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
+    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_ESC,
+    CTL_Z,   ALT_X,   FN3_C,   FN4_V,   FN2_BSPC,FN1_SPC, FN5_B,   ALT_N,   CTL_M,   SFT_ENT
+  ),
+
+  [1] = LAYOUT_ortho_3x10(
+    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
+    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
+    _______, _______, _______, _______, KC_DEL,  _______, _______, _______, _______, _______
+  ),
+
+  [2] = LAYOUT_ortho_3x10(
+    KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,
+    KC_F11,  KC_F12,  _______, _______, _______, _______, _______, _______, _______, KC_GRV,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+  ),
+
+  [3] = LAYOUT_ortho_3x10(
+    _______, _______, _______, _______, _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
+    KC_TAB,  _______, _______, _______, _______, KC_COMM, KC_DOT,  KC_SLSH, KC_SCLN, KC_QUOT,
+    _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+  ),
+
+  [4] = LAYOUT_ortho_3x10(
+    _______, _______, _______, _______, _______, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,
+    KC_TAB,  _______, _______, _______, _______, KC_LABK, KC_RABK, KC_QUES, KC_COLN, KC_DQUO,
+    _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END
+  ),
+
+  [5] = LAYOUT_ortho_3x10(
+    KC_CALC, KC_WHOM, KC_MAIL, KC_MYCM, _______, _______, RGB_VAI, RGB_VAD, BL_DEC, BL_INC,
+    _______, _______, _______, _______, _______, _______, RGB_MOD, RGB_RMOD, _______, _______,
+    _______, _______, _______, _______, RESET,   _______, _______, RGB_HUD, _______, _______
+  )
+
+};
+
+#ifdef OLED_ENABLE
+
+bool oled_task_user(void) {
+    // Host Keyboard Layer Status
+    oled_write_P(PSTR("Layer: "), false);
+
+    switch (get_highest_layer(layer_state)) {
+        case 0:
+            oled_write_P(PSTR("QWERTY\n"), false);
+            break;
+        case 1:
+            oled_write_P(PSTR("NUMBERS\n"), false);
+            break;
+        case 5:
+            oled_write_P(PSTR("RGB_LEDS\n"), false);
+            break;
+        default:
+            // Or use the write_ln shortcut over adding '\n' to the end of your string
+            oled_write_ln_P(PSTR("Undefined"), false);
+    }
+
+    // Host Keyboard LED Status
+    led_t led_state = host_keyboard_led_state();
+    oled_write_P(led_state.num_lock ? PSTR("NUM ") : PSTR("    "), false);
+    oled_write_P(led_state.caps_lock ? PSTR("CAPSLOCK ") : PSTR("    "), false);
+    oled_write_P(led_state.scroll_lock ? PSTR("SCR ") : PSTR("    "), false);
+
+    return false;
+}
+
+#endif
